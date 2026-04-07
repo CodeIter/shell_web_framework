33f7f155f1784aca26f10375230c1b16f64222e6
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-04-07 04:22:16 +0100

    Chore: update getfiles.sh
    Signed-off-by: Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>

--- getfiles.sh
+++ getfiles.sh
@@ -5,7 +5,7 @@ shopt -s globstar
 {
 tree -a -I .git --gitignore
 echo
-for f in ./.env* ./*.bash ./middleware.d/* ./router.d/**/* "${@}" ; do
+for f in ./.env* ./**/*.bash "${@}" ; do
   [[ -f "$f" ]] || continue
   echo "-------------------------------------------------------------------"
   echo "File: $f"
