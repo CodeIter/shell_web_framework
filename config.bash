c0ff2eb5ed438714f908eac8b882abed07fa07fd
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-04-08 16:55:34 +0100

    Fix: update config
    Signed-off-by: Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>

--- config.bash
+++ config.bash
@@ -7,7 +7,8 @@ set -euo pipefail
 export HOST="${HOST:-127.0.0.1}"
 export PORT="${PORT:-8080}"
 
-export SESSION_DIR="${SESSION_DIR:-${PWD}/sessions}"
+export SESSION_DIR="${SESSION_DIR:-sessions}"
+export SESSION_TTL="${SESSION_TTL:-86400}" # 1 day
+
 export SESSION_SECRET="${SESSION_SECRET:-change-this-secret-in-env}"
-export SESSION_TTL="${SESSION_TTL:-86400}"   # 1 day
 
