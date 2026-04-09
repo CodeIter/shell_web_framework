628c19f623098aba8cb59a8d9ae0a06d921c47da
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-04-09 02:41:34 +0100

    Fix: fix middleware.d/00-request-logger.bash
    Signed-off-by: Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>

--- middleware.d/00-request-logger.bash
+++ middleware.d/00-request-logger.bash
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
->&2 echo "$(date) ${REMOTE_ADDR} :${REMOTE_PORT}" ${METHOD} ${WEBPATH}
+>&2 echo "$(date) ${REMOTE_ADDR} :${REMOTE_PORT} ${REMOTE_HOST} ${REMOTE_PROTO} ${METHOD} ${WEBPATH}"
 
